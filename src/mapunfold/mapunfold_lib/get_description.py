519ce5c97b71d02311f22528db90b15acb43d828
thehowtoproject <thehowtoproject123@gmail.com>
2025-08-23 16:42:35 +0200

    latest

--- src/mapunfold/mapunfold_lib/get_description.py
+++ src/mapunfold/mapunfold_lib/get_description.py
@@ -1,8 +1,37 @@
 from .description import Description
 
 
-async def gather_info(id):
+def get_open_transport_info():
     pass
+
+
+def get_osm_info():
+    pass
+
+
+def get_insa_export_info():
+    pass
+
+
+def pull_info_together(info_open_transport_data, info_open_street_map, info_insa_export_api):
+    pass
+
+
+async def gather_info(id):
+
+
+    info_open_transport_data = get_open_transport_info()
+
+    info_open_street_map = get_osm_info()
+
+    info_insa_export_api = get_insa_export_info()
+
+
+
+
+
+    info = pull_info_together(info_open_transport_data, info_open_street_map, info_insa_export_api)
+    return info
 
 
 async def create_texts(info):
